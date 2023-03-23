e41d0e9169f2d6c204082795d73ae1c1dfb988f6
A-R96 <andrewrush1@hotmail.com>
2023-03-23 17:15:35 -0400

    Update main

--- main.py
+++ main.py
@@ -1,71 +1,123 @@
-from dotenv import load_dotenv
+import argparse
+import logging
+import os
+import sys
+
 import pandas as pd
 import requests
-import sys
-import os
+from dotenv import load_dotenv
 from rich.console import Console
 
-console = Console()
 
-# Load the URL and TOKEN from .env
+console = Console()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 load_dotenv()
-URL = os.getenv('URL')
-TOKEN = os.getenv('TOKEN')
+URL = os.getenv("URL")
+TOKEN = os.getenv("TOKEN")
 
 
 def get_devices(token, url):
+    """Get a list of devices from the LibreNMS API.
 
-    # Send the request to the API
+    Args:
+        token (str): API token.
+        url (str): API URL.
+
+    Returns:
+        pd.DataFrame: DataFrame containing device information.
+    """
     try:
-        headers = {'X-Auth-Token': token}
-        devices_url = f'{url}/api/v0/devices/'
+        headers = {"X-Auth-Token": token}
+        devices_url = f"{url}/api/v0/devices/"
         response = requests.get(devices_url, headers=headers)
         response.raise_for_status()
         response = response.json()
 
-        # If the API gives an ok code. Get all the devices and the values we want
-        if response['status'] == 'ok':
+        if response["status"] == "ok":
             console.log(f'Success - LibreNMS Devices count: {response["count"]}\n')
-            devices = pd.json_normalize(response['devices'])
-            devices = devices.sort_values('hostname').reset_index(drop=True)
-            selected_cols = ['hostname', 'device_id', 'hardware', 'os', 'ip', 'sysName',
-                             'sysObjectID', 'serial', 'location', 'location_id', 'uptime',
-                             'sysDescr', 'status', 'last_polled_timetaken']
+            devices = pd.json_normalize(response["devices"])
+            devices = devices.sort_values("hostname").reset_index(drop=True)
+            selected_cols = [
+                "hostname",
+                "device_id",
+                "hardware",
+                "os",
+                "ip",
+                "sysName",
+                "serial",
+                "location",
+                "sysDescr",
+            ]
             return devices[selected_cols]
 
         else:
-            console.print('Failed to get LibreNMS Devices list via API', style='bold red')
-            sys.exit(1)
+            logger.error("Failed to get LibreNMS Devices list via API")
+            raise requests.exceptions.RequestException(response["message"])
 
     except requests.exceptions.RequestException as e:
-        console.print(f'An error occurred: {e}', style='bold red')
-        sys.exit(1)
+        logger.exception("An error occurred")
+        raise
+
+
+def export_devices(df, output_format, output_file):
+    """Export a DataFrame to a file.
+
+    Args:
+        df (pd.DataFrame): DataFrame to export.
+        output_format (str): Output format ('csv' or 'excel').
+        output_file (str): Output file name.
+    """
+    if output_format == "csv":
+        output_file = (
+            output_file if output_file.endswith(".csv") else f"{output_file}.csv"
+        )
+        df.to_csv(output_file, index=False)
+    elif output_format == "excel":
+        output_file = (
+            output_file if output_file.endswith(".xlsx") else f"{output_file}.xlsx"
+        )
+        with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
+            df.to_excel(writer, sheet_name="Sheet1", index=False)
+    else:
+        raise ValueError(f"Invalid output format: {output_format}")
 
 
 def main():
-    # Main function that runs the full script
+    parser = argparse.ArgumentParser(
+        description="Get a list of devices from the LibreNMS API."
+    )
+    parser.add_argument(
+        "--format",
+        choices=["csv", "excel"],
+        default="excel",
+        help="Output format (default: excel)",
+    )
+    parser.add_argument(
+        "--outfile",
+        default="device_list.xlsx",
+        help="Output file name (default: device_list.xlsx)",
+    )
+    args = parser.parse_args()
+
     try:
-        console.rule('Getting LibreNMS Devices from API')
-        console.log(f'LibreNMS URL: {URL}', '\n')
+        console.rule("Getting LibreNMS Devices from API")
+        console.log(f"LibreNMS URL: {URL}\n")
 
         # Get devices
         df_devices = get_devices(TOKEN, URL)
-        
-        # Export device list to excel sheet
-        with pd.ExcelWriter('device_list.xlsx', engine='xlsxwriter') as writer:
-            df_devices.to_excel(writer, sheet_name='Sheet1', index=False)
-            
-        # Alternatevly write to a csv
-        #df_devices.to_csv('devices.csv')
+
+        # Export the devices to a file
+        export_devices(df_devices, args.format, args.outfile)
 
         # Print the DataFrame to the console
         print(df_devices.tail(6))
         sys.exit(0)
 
     except Exception as e:
-        console.print(f'An error occurred: {e}', style='bold red')
+        console.print(f"An error occurred: {e}", style="bold red")
         sys.exit(1)
-
 
 
 main()
